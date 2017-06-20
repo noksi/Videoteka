58ff327666b748dd5e432c7450de834369015998
gorant696 <axelfoley@net.hr>
2017-06-20 17:45:35 +0200

    flex

--- flexlogin.php
+++ flexlogin.php
@@ -6,11 +6,18 @@
         <span><strong>Korisničko ime:</strong></span><br>
         <input class="form-control2" type="text" name="username"><br>
         <span><strong>Lozinka:</strong></span><br>
-        <input class="form-control2" type="password" name="password"><br>
+        <input class="form-control2" type="password" name="password"><br><br>
         <input type="submit" name="submit" class="btn btn-default butoni" value="Prijavi se" >
     
 </form> 
     
+    <br>
+    <br>
+    <br>
+    <form action="#" method="POST" class="signup">
+        <span><strong>Niste se još registrirali?</strong></span><br><br>
+        <input type="submit" name="submitreg" class="btn btn-default butoni" value="Registriraj se" >
+    </form>
   </div> <!--korisni2-->
 
 
