7dcc3d15c078534eccc5dfa57bca0f211f562345
gorant696 <axelfoley@net.hr>
2017-06-21 15:29:08 +0200

    flexlogin, reg okviri za prijavu

--- flexlogin.php
+++ flexlogin.php
@@ -1,19 +1,24 @@
 <div class="flex-container">
 
 <div class="korisni2">
- 
+    
+    
+    
     <form method="POST" action="process.php" class="signup">
-        <span><strong>Korisničko ime:</strong></span><br>
+        <span style="color: #9d9d9d !important;"><strong>PRIJAVI SE</strong></span>
+        <div class="flexuserlogin">
+        <span style="color: #222 !important;"><strong>Korisničko ime:</strong></span><br>
         <input class="form-control2" type="text" name="username"><br>
-        <span><strong>Lozinka:</strong></span><br>
-        <input class="form-control2" type="password" name="password"><br><br>
+        <span style="color: #222 !important;"><strong>Lozinka:</strong></span><br>
+        <input class="form-control2" type="password" name="password">
+        <br><br>
         <input type="submit" name="submit" class="btn btn-default butoni" value="Prijavi se" >
     
 </form> 
+    </div> <!--flexuserlogin-->
+    <br>
+    <br>
     
-    <br>
-    <br>
-    <br>
     <form action="reg.php" method="POST" class="signup">
         <span><strong>Niste se još registrirali?</strong></span><br><br>
         <input type="submit" name="submitreg" class="btn btn-default butoni" value="Registriraj se" >
--- reg.php
+++ reg.php
@@ -9,22 +9,24 @@
 <div class="korisni2">
  
     <form method="POST" action="php/regscript.php" class="signup" enctype="multipart/form-data">
-        
-        <span><strong>Unesite željeno korisničko ime:</strong></span><br>
+        <span><strong>REGISTRIRAJ SE</strong></span><br>
+        <div class="flexuserlogin">
+            <span><strong style="color: #222">Unesite željeno korisničko ime:</strong></span><br>
         <input class="form-control2" type="text" name="korisnik"><br>
         
-        <span><strong>Unesite željenu lozinku:</strong></span><br>
+        <span><strong style="color: #222">Unesite željenu lozinku:</strong></span><br>
         <input class="form-control2" type="password" name="lozinka"><br><br>
         
-        <span><strong>Odaberite vašu profilnu sličicu:</strong></span>
+        <span><strong style="color: #222">Odaberite vašu profilnu sličicu:</strong></span>
         <input type="file" class="file" name="profilnaslika" id="profilnaslika">
         <label for="profilnaslika"><span class="glyphicon glyphicon-search"></span></label><br><br>
         
         <input type="submit" name="registracija" class="btn btn-default butoni" value="Podnesi registraciju">
     
 </form> 
+</div> <!--flexuserlogin-->
     
-    
+<br>
     <form action="index.php" method="POST" class="signup">
         
          <input type="submit" name="homeindex" class="btn btn-default butoni" value="Vrati se na početnu stranicu" title="Početna stranica">
