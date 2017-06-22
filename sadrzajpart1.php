73c2cd004bac312f8d86687f9f08a2d124c84ab2
gorant696 <axelfoley@net.hr>
2017-06-22 18:06:33 +0200

    sort glifovi

--- sadrzajpart1.php
+++ sadrzajpart1.php
@@ -6,9 +6,9 @@
 <tr>
 	
 
-<th class="tha"><a href='sortfilmovi.php'>Naziv filma</a></th>
-<th class ="tha"><a href="sortgodina.php">Godina</a></th>
-<th class ="tha"><a href="sortzanrovi.php">Žanr</a></th>
+<th class="tha"><a href='sortfilmovi.php'>Naziv filma</a> <a href="sortfilmovi.php" class="glyphicon glyphicon-arrow-down" title="Sortiraj po nazivu filma"></a></th>
+<th class ="tha"><a href="sortgodina.php">Godina</a> <a href="sortgodina.php" class="glyphicon glyphicon-arrow-down" title="Soritraj po godini"></a></th>
+<th class ="tha"><a href="sortzanrovi.php">Žanr</a> <a href="sortzanrovi.php" class="glyphicon glyphicon-arrow-down" title="Sortiraj po žanrovima"></a></th>
 <th>Redatelj</th>
 <th>Cover</th>
 <th>Uređivanje</th>
