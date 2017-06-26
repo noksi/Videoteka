04abfec81d5909bd92b9dd07eb6de632efddb749
gorant696 <axelfoley@net.hr>
2017-06-26 15:40:32 +0200

    pmfolder

--- pmfolder.php
+++ pmfolder.php
@@ -22,13 +22,19 @@
             <ul class="pm" style="width:100%">
                 <div class="caption tha" style="font-family: Play, sans-serif">Privatne poruke od <?php echo $_SESSION['username']; ?></div>
                 
+                <a href="#" class="glyphicon glyphicon-trash pmglif" title="Obriši označene poruke"></a>
+                <a href="#" class="glyphicon glyphicon-pencil pmglifnew" title="Nova poruka"></a>
+                <a href="pmfolder.php" class="glyphicon glyphicon-inbox pmglifinbox" title="Dolazne poruke"></a>
+                <a href="#" class="glyphicon glyphicon-send pmglifsend" title="Poslane poruke"></a>
+                <br><br>
+                
                 <?php $querypm="select * from pm inner join login on login.userid=pm.otheruserid where pm.userid='".$_SESSION['userid']."'";
         $resultpm=mysqli_query($conn, $querypm);
         while ($rowpm=mysqli_fetch_assoc($resultpm)) {?>
                 <a href="#" class="pmfolder"><li class="tr" style="padding-top: 3px; padding-bottom: 3px;">
-                        Od: <span style="color:black; padding-left:10px;"><?php echo $rowpm['username']; ?></span> <br>
-                        Title: <span style="color:black; padding-left:10px;"><?php echo $rowpm['titlemsg']; ?></span><br>
-                        Datum: <span style="color:black; padding-left:10px;"><?php echo $rowpm['datum']; ?></span>
+                        Od: <span style="color:black; padding-left:10px; font-size: 80%; font-weight: bold;"><?php echo $rowpm['username']; ?></span> <br>
+                        Title: <span style="color:black; padding-left:10px; font-size: 80%; font-weight: normal;"><?php echo $rowpm['titlemsg']; ?></span><br>
+                        Datum: <span style="color:black; padding-left:10px; font-size: 80%; font-weight: normal;"><?php echo $rowpm['datum']; ?></span>
         </li></a> <?php } ?>
             </ul>
             
