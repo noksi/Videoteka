dbe1426dbb6f2e7b23fd6a594dff3d1f83943765
gorant696 <axelfoley@net.hr>
2017-06-26 21:36:36 +0200

    read pm, deletanje iz reada

--- pmfolder.php
+++ pmfolder.php
@@ -17,7 +17,7 @@
                 <div class="tablahead">
                     <form method="POST" action="php/deletepm.php">
                         <ul class="pm" style="width:100%">
-                            <div class="caption tha" style="font-family: Play, sans-serif">Privatne poruke od <?php echo $_SESSION['username']; ?></div>
+                            <div class="caption tha" style="font-family: Play, sans-serif">Dolazne poruke</div>
 
                             <label for="deletepm" class="glyphicon glyphicon-trash pmglif" title="Obriši označene poruke"></label>
                             <input name="deletepm" id="deletepm" type="submit" style="opacity: 0; width: 0.1px; height: 0.1px;  overflow: hidden; position: absolute;  z-index: -1;">
@@ -32,7 +32,7 @@
                             while ($rowpm = mysqli_fetch_assoc($resultpm)) {
                                 ?>
                                 <li class="tr pmfolder" style="padding-top: 3px; padding-bottom: 3px;">
-                                        Od:  <span style="color:black; padding-left:10px; font-size: 80%; font-weight: bold;"><?php echo $rowpm['username']; ?><a href="readpm.php" class="glyphicon glyphicon-envelope glifinbox" title="Pročitajte poruku"></a></span><br>
+                                        Od:  <span style="color:black; padding-left:10px; font-size: 80%; font-weight: bold;"><?php echo $rowpm['username']; ?><a href="readpm.php?pmid=<?php echo $rowpm['pmid']; ?>" class="glyphicon glyphicon-envelope glifinbox" title="Pročitajte poruku"></a></span><br>
                                         Title: <span style="color:black; padding-left:10px; font-size: 80%; font-weight: normal;"><?php echo $rowpm['titlemsg']; ?></span><br>
                                         Datum: <span style="color:black; padding-left:10px; font-size: 80%; font-weight: normal;"><?php echo $rowpm['datum']; ?></span> 
                                     <input type="checkbox" name="checkdelete[]" value="<?php echo $rowpm['pmid']; ?>"></li>  <?php } ?>
--- readpm.php
+++ readpm.php
@@ -15,27 +15,35 @@
 
             <div class="sadrzaj">
                 <div class="tablahead">
-                    <form method="POST" action="php/deletepm.php">
+                    <form method="POST" action="php/deletepmid.php">
                     <ul class="pm" style="width:100%">
-                        <div class="caption tha" style="font-family: Play, sans-serif">Privatne poruke od <?php echo $_SESSION['username']; ?></div>
+                        <div class="caption tha" style="font-family: Play, sans-serif">Privatna poruka</div>
 
-                        <label for="deletepm" class="glyphicon glyphicon-trash pmglif" title="Obriši ovu poruku"></label>
-                        <input name="deletepm" id="deletepm" type="submit" style="opacity: 0; width: 0.1px; height: 0.1px;  overflow: hidden; position: absolute;  z-index: -1;">
+                        <label for="deletepmid" class="glyphicon glyphicon-trash pmglif" title="Obriši ovu poruku"></label>
+                        <input name="deletepmid" id="deletepmid" type="submit" style="opacity: 0; width: 0.1px; height: 0.1px;  overflow: hidden; position: absolute;  z-index: -1;">
                         <a href="newpm.php" class="glyphicon glyphicon-pencil pmglifnew" title="Nova poruka"></a>
                         <a href="pmfolder.php" class="glyphicon glyphicon-inbox pmglifinbox" title="Dolazne poruke"></a>
                         <a href="#" class="glyphicon glyphicon-send pmglifsend" title="Poslane poruke"></a>
                         <br><br>
 
     <?php
-    $querypm = "select * from pm inner join login on login.userid=pm.otheruserid where pm.userid='" . $_SESSION['userid'] . "'";
+    $querypm = "select * from pm inner join login on login.userid=pm.otheruserid where pm.userid='".$_SESSION['userid']."' and pmid='".$_GET['pmid']."'";
     $resultpm = mysqli_query($conn, $querypm);
     while ($rowpm = mysqli_fetch_assoc($resultpm)) {
         ?>
                             <li class="pmfolder" style="padding-top: 3px; padding-bottom: 3px;">
+                                <input type="hidden" name="hiddenpmid" value="<?php echo $rowpm['pmid']; ?>">
                                     Od: <span style="color:black; padding-right:10px; font-size: 80%; font-weight: bold;"><?php echo $rowpm['username']; ?></span> 
                                     Title: <span style="color:black; padding-right:10px; font-size: 80%; font-weight: normal;"><?php echo $rowpm['titlemsg']; ?></span>
                                     Datum: <span style="color:black; padding-right:10px; font-size: 80%; font-weight: normal;"><?php echo $rowpm['datum']; ?></span> <input type="hidden" name="inboxdelete" value="<?php echo $rowpm['pmid']; ?>">
-                                </li> <?php } ?>
+                                </li> 
+                                <li class="pmfolder" style="padding-top: 3px; padding-bottom: 3px;">
+                                    <br><span style="font-family: Play, sans-serif; font-size:120% !important;">Sadržaj</span>:
+                                </li>
+                                <li class="pmfolder" style="padding-top: 3px; padding-bottom: 3px;">
+                                    <span style="color: black;"><?php echo nl2br($rowpm['msg']); ?></span>
+                                </li>
+                                    <?php } ?>
                     </ul>
                          
                     </form>
