6910cb4310aa06e1b02f3f17270357019da85468
gorant696 <axelfoley@net.hr>
2017-06-27 15:53:03 +0200

    font awesome, headpart

--- headpart.php
+++ headpart.php
@@ -9,6 +9,8 @@
  <link rel="stylesheet" href="css/bootstrap.min.css">
  <link rel="stylesheet" href="css/bootstrap-theme.css">
  <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+ <link rel="stylesheet" href="css/font-awesome.css">
+ <link rel="stylesheet" href="css/font-awesome.min.css">
  <link rel="stylesheet" href="css/style.css">
 
 </head>
