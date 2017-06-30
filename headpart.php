dd86d80ed017f499c8ef75b990f03a4211c159f1
gorant696 <axelfoley@net.hr>
2017-06-30 02:37:40 +0200

    header

--- headpart.php
+++ headpart.php
@@ -4,7 +4,6 @@
  <meta charset="utf-8">
  <meta name="viewport" content="width=device-width, initial-scale=1">
  <meta name="viewport" content="height=device-height, initial-scale=1">
-<link href="https://fonts.googleapis.com/css?family=Play" rel="stylesheet"> <!--font za logo u headeru-->
  <link rel="stylesheet" href="css/bootstrap.css">
  <link rel="stylesheet" href="css/bootstrap.min.css">
  <link rel="stylesheet" href="css/bootstrap-theme.css">
